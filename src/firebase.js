b71127cb1c90980e2153862f0b9ac3d6dfee7aa4
Carol Enache <enache.carol@gmail.com>
2017-10-18 23:18:12 +0100

    Link app to Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  storageBucket: '',
-  messagingSenderId: ''
+  apiKey: "AIzaSyDxAF9oJMKRiZ8DRRcuYeLdvZFebXsAf3E",
+  authDomain: "lunch-rush-23bb7.firebaseapp.com",
+  databaseURL: "https://lunch-rush-23bb7.firebaseio.com",
+  projectId: "lunch-rush-23bb7",
+  storageBucket: "lunch-rush-23bb7.appspot.com",
+  messagingSenderId: "155251745525"
 };
 
 firebase.initializeApp(config);
